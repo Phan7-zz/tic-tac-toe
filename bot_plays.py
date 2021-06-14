62a1e60d6c44c7a3675ca977936e42fddedb2d76
/Phant <74624003+Phan7@users.noreply.github.com>
2021-06-14 18:05:22 -0300

    Update bot_plays.py

--- bot_plays.py
+++ bot_plays.py
@@ -4,6 +4,12 @@ groups = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 4, 1], [8, 5, 2], [9, 6, 3], [7, 
 corners = [1, 3, 7, 9]
 r_corners = [9, 7, 3, 1]
 middles = [2, 4, 6, 8]
+opposites = {
+    2: [7, 9],
+    4: [3, 9],
+    6: [1, 7],
+    8: [1, 3]
+}
 plays = 1
 bot_last_move = 0
 strategy = 0
@@ -37,7 +43,7 @@ def bot_turn_1(board: dict):
 def bot_turn_2(board: dict):
     # Level 2 of difficulty
     # This level will draw if make a random play or a kinda smart play
-    smart = choice(1, 2) # 1 means smart move, 2 means not smart move
+    smart = choice([1, 2]) # 1 -> smart move / 2 -> not smart move
 
     if smart == 1:
         # Check if someone is near to a win
@@ -61,19 +67,25 @@ def bot_turn_2(board: dict):
     return board
 
 def bot_turn_3(board: dict, last_move: int):
-    global plays, groups, bot_last_move, corners, r_corners, strategy
+    global plays, groups, bot_last_move, corners, r_corners, strategy, opposites
+    # Strategy 1 and 2 is when the bot is startin the game
+    # ↳ Strategy 1 -> Start from the cernter of the board
+    # ↳ Strategy 2 -> Start from the corner of the board
 
-    if last_move == 0: # Means that the bot is starting the game
+    if last_move == 0: # It means that the bot is starting the game
         strategy = choice([1, 2])
     
-    elif last_move != 0 and plays == 1:
+    elif last_move != 0 and plays == 1: # It means that the player has already started the game
+        # So we have to check if the player moved to 5, if yes, we have a problem, so, strategy 3
         if last_move == 5:
             strategy = 3
+
+        # If the player didn't moved to 5, we had luck, so, strategy 4
         else:
             strategy = 4
 
     if strategy == 1: # The bot is starting, and the strategy is start from center
-        if plays == 1:
+        if plays == 1: # If it's the first move
             board[5] = 'X'
             plays += 1
             bot_last_move = 5
@@ -81,40 +93,21 @@ def bot_turn_3(board: dict, last_move: int):
     
         elif plays == 2: # If this is the second bot's move
             if last_move in corners: # If the player's first move was in the corners
-                move = r_corners[corners.index(last_move)]
+                move = r_corners[corners.index(last_move)] # The bot will find the oposite side of the player's move
                 board[move] = 'X'
                 plays += 1
                 bot_last_move = move
                 return board
                 
-            else: # If the player's first move was in the middles
-                if last_move == 4:
-                    move = choice([9, 3])
-                    board[move] = 'X'
-                    plays += 1
-                    bot_last_move = move
-                    return board
-                elif last_move == 8:
-                    move = choice([1, 3])
-                    board[move] = 'X'
-                    plays += 1
-                    bot_last_move = move
-                    return board
-                elif last_move == 6:
-                    move = choice([1, 7])
-                    print(move)
-                    board[move] = 'X'
-                    plays += 1
-                    bot_last_move = move
-                    return board
-                elif last_move == 2:
-                    move = choice([7, 9])
-                    board[move] = 'X'
-                    plays += 1
-                    bot_last_move = move
-                    return board
+            else: # If the player's first move was in the middles, the bot will win
+                move = choice(opposites[last_move])
+                board[move] = 'X'
+                plays += 1
+                bot_last_move = move
+                return board
 
         elif plays == 3: # If this is the third bot's move
+            # First, the bot will check if someone is near to a win
             for possibility in groups:
                 group = (board[possibility[0]], board[possibility[1]], board[possibility[2]])
                 if group.count('X') == 2 and group.count(' ') == 1: # Means that the bot is near to a win
@@ -123,71 +116,40 @@ def bot_turn_3(board: dict, last_move: int):
                     bot_last_move = move
                     board[possibility[move]] = 'X'
                     return board
-            
-            for possibility in groups:
-                group = (board[possibility[0]], board[possibility[1]], board[possibility[2]])
-                if group.count('O') == 2 and group.count(' ') == 1: # Means that the player is near to a win
+                elif group.count('O') == 2 and group.count(' ') == 1: # Means that the player is near to a win
                     move = group.index(' ') # Finding where to block the player
                     board[possibility[move]] = 'X'
                     plays += 1
                     bot_last_move = move
                     return board
             
-            if last_move == 4:
-                if board[9] == ' ':
-                    board[9] = 'X'
+            # If arrived here, so nobody is near to a win
+            # The bot will move to the opposite side of the player's play
+            for b in choice([[0, 1], [1, 0]]):
+                if board[opposites[last_move][b]] == ' ':
+                    board[opposites[last_move][b]] = 'X'
                     plays += 1
-                    bot_last_move = 9
-                    return board
-                elif board[3] == ' ':
-                    board[3] = 'X'
-                    plays += 1
-                    bot_last_move = 3
-                    return board
-            elif last_move == 8:
-                if board[1] == ' ':
-                    board[1] = 'X'
-                    plays += 1
-                    bot_last_move = 1
-                    return board
-                elif board[3] == ' ':
-                    board[3] = 'X'
-                    plays += 1
-                    bot_last_move = 3
-                    return board
-            elif last_move == 6:
-                if board[1] == ' ':
-                    board[1] = 'X'
-                    plays += 1
-                    bot_last_move = 1
-                    return board
-                elif board[7] == ' ':
-                    board[7] = 'X'
-                    plays += 1
-                    bot_last_move = 7
-                    return board
-            elif last_move == 2:
-                if board[7] == ' ':
-                    board[7] = 'X'
-                    plays += 1
-                    bot_last_move = 7
-                    return board
-                elif board[9] == ' ':
-                    board[9] = 'X'
-                    plays += 1
-                    bot_last_move = 9
+                    bot_last_move = opposites[last_move][b]
                     return board
         
-        else:
+        else: # If arrived here, it means that the game will end in a draw
+            # Check if someone is near to a win
             for possibility in groups:
                 group = (board[possibility[0]], board[possibility[1]], board[possibility[2]])
                 if group.count('X') == 2 and group.count(' ') == 1: # Means that the bot is near to a win
                     move = group.index(' ') # Finding where to move to win
+                    plays += 1
+                    bot_last_move = move
+                    board[possibility[move]] = 'X'
+                    return board
+                elif group.count('O') == 2 and group.count(' ') == 1: # Means that the player is near to a win
+                    move = group.index(' ') # Finding where to block the player
                     board[possibility[move]] = 'X'
                     plays += 1
                     bot_last_move = move
                     return board
 
+        # If arrived here, the bot will meka a random play, because the game will end in a draw
         return bot_turn_1(board)
 
     elif strategy == 2: # The bot is starting, and the strategy is start from the corners
